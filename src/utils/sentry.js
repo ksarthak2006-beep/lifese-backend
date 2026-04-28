a5ac7f523ad52119d7bdd471d0cb58081684b6c0
ksarthak2006-beep <ksarthak2006@gmail.com>
2026-04-28 23:45:48 +0530

    deploy: add src/utils/sentry.js

---
+++ src/utils/sentry.js
@@ -0,0 +1,15 @@
+import * as Sentry from '@sentry/node';
+
+export const initSentry = () => {
+    if (process.env.SENTRY_DSN) {
+        Sentry.init({
+            dsn: process.env.SENTRY_DSN,
+            environment: process.env.NODE_ENV || 'development',
+            tracesSampleRate: 1.0,
+        });
+        return true;
+    }
+    return false;
+};
+
+export default Sentry;
